2f98d57ba9e3e6f885af2fc0df35a524cf7f0ba5
Steven Lian <stevenlian1994@gmail.com>
2024-01-05 03:17:06 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, command-center');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Command-center');
   });
 });
